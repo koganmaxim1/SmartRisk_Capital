376bcb16c9462dcd7dc68dae42427993dba76670
koganmaxim1 <kogan.maxim1@gmail.com>
2025-04-24 14:00:48 +0300

    stock search fix

--- client/src/components/UserPreferences/UserPreferences.jsx
+++ client/src/components/UserPreferences/UserPreferences.jsx
@@ -259,7 +259,7 @@ export default function UserPreferences() {
                         )}
                         showSearch
                         filterOption={(input, option) =>
-                          option.label.toLowerCase().includes(input.toLowerCase())
+                          option.label.toLowerCase().startsWith(input.toLowerCase())
                         }
                       />
                       <InfoCircleOutlined
