5aaa42738f4f72e3cc4577ce4b02212064ba0c11
koganmaxim1 <kogan.maxim1@gmail.com>
2025-05-15 13:40:28 +0300

    Update main.py

--- server/main.py
+++ server/main.py
@@ -29,11 +29,21 @@ def get_calculate_spread_stocks(request: StockRequest):
 
 @app.get("/stocks/GetAllStocksData")
 def get_stocks_data():
-    # Load fresh data for each request
-    stocks_data_df = pd.read_csv(excel_file_path)
-    cleaned_df = stocks_data_df.replace([float('inf'), float('-inf')], 0).fillna(0)
-    stocks_data = cleaned_df.to_dict(orient='records')
-    return JSONResponse(content=stocks_data)
+    import requests
+    import tempfile
+
+    xls_url = "https://lhacesogkispjqlndfch.supabase.co/storage/v1/object/public/excel-files/00557070.xlsx"
+    response = requests.get(xls_url)
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as tmp_file:
+        tmp_file.write(response.content)
+        tmp_path = tmp_file.name
+
+    xls = pd.ExcelFile(tmp_path)
+    df = pd.read_excel(xls, sheet_name=0)  # או sheet_name="Stocks" אם יש כזה
+    df = df.replace({float("inf"): 0, float("-inf"): 0}).fillna(0)
+    return JSONResponse(content=df.to_dict(orient="records"))
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
